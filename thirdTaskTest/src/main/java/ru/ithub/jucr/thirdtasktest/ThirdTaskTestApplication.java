67aaab49e56c3b49eee8acf04d7f7bdafbf200bf
Ju3r <96393329+Ju3r@users.noreply.github.com>
2023-04-22 03:27:14 +0300

    Add changes and add unit-tests for service

---
+++ thirdTaskTest/src/main/java/ru/ithub/jucr/thirdtasktest/ThirdTaskTestApplication.java
@@ -0,0 +1,11 @@
+package ru.ithub.jucr.thirdtasktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdTaskTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdTaskTestApplication.class, args);
+    }
+}
---
+++ thirdTaskTest/src/test/java/ru/ithub/jucr/thirdtasktest/ThirdTaskTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ithub.jucr.thirdtasktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdTaskTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
