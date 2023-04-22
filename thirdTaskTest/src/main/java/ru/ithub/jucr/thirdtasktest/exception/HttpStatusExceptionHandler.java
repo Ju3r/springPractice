70f7da9280ccc142b7d52cc26d6cf1f00d408502
Ju3r <96393329+Ju3r@users.noreply.github.com>
2023-04-22 10:44:04 +0300

    Add changes to pom.xml and add unit-tests for service

--- thirdTaskTest/src/main/java/ru/ithub/jucr/thirdtasktest/exception/HttpStatusExceptionHandler.java
+++ thirdTaskTest/src/main/java/ru/ithub/jucr/thirdtasktest/exception/HttpStatusExceptionHandler.java
@@ -1,9 +1,17 @@
 package ru.ithub.jucr.thirdtasktest.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.ithub.jucr.secondtask.restcontrollerservice.model.dto.validation.ValidationError;
+import ru.ithub.jucr.secondtask.restcontrollerservice.model.dto.validation.ValidationErrorResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class HttpStatusExceptionHandler {
@@ -13,6 +21,17 @@ public class HttpStatusExceptionHandler {
         HttpStatusEnum httpStatusEnum = ex.getHttpStatusEnum();
         HttpStatusEnumExceptionResponse response = new HttpStatusEnumExceptionResponse(httpStatusEnum);
         return new ResponseEntity<>(response, httpStatusEnum.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ValidationErrorResponse onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final List<ValidationError> validationErrors = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new ValidationError(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(validationErrors);
     }
 
 
